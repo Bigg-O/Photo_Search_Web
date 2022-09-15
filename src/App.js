4f43d839c2789a5b907069084b5519d44799392e
wookeun song <wookeun.s@gmail.com>
2022-09-14 22:22:30 -0400

    API fetch set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import SearchBar from './Components/SearchBar'
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const PEXELS_PHOTOS_URL = "https://api.pexels.com/v1/curated"
+
+
+
+export class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      page: 1,
+      photos: [],
+      // isLoading: false
+    }
+  }
+
+  handleDataLoad = () => {
+    axios
+      .get(PEXELS_PHOTOS_URL + `?page=${this.page}&per_page=10`, {
+        headers: {
+          Authorization: process.env.REACT_APP_PEXELS_KEY
+        }
+      }).then(resp => {
+        this.setState({ photos: resp.data.photos })
+      }).catch(err => {
+        console.log(err);
+      });
+  }
+  
+  render() { 
+    return (
+      <SearchBar/>
+
+    }
+  }
+  
 
 export default App;
