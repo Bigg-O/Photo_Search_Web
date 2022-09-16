b1c79cb3ed6a17a6964365d38a1828b8e17f4b24
wookeun song <wookeun.s@gmail.com>
2022-09-16 00:10:50 -0400

    pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 import SearchBar from './Components/SearchBar'
+import PaginationBar from './Components/PaginationBar'
+import PhotoContainer from './Containers/PhotoContainer'
 import './App.css';
 import axios from "axios";
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const PEXELS_PHOTOS_URL = "https://api.pexels.com/v1/curated"
-
-
+const PER_PAGE = 10
 
 export class App extends Component {
   constructor() {
     super()
     this.state = {
-      page: 1,
+      currentPage: 1,
       photos: [],
-      // isLoading: false
     }
   }
 
-  handleDataLoad = () => {
+  componentDidMount() {
+    this.handleDataLoad(this.state.currentPage)
+  }
+
+  handleDataLoad = pageNum => {
     axios
-      .get(PEXELS_PHOTOS_URL + `?page=${this.page}&per_page=10`, {
+      .get(PEXELS_PHOTOS_URL + `?page=${pageNum}&per_page=${PER_PAGE}`, {
         headers: {
           Authorization: process.env.REACT_APP_PEXELS_KEY
         }
@@ -29,13 +35,26 @@ export class App extends Component {
         console.log(err);
       });
   }
+
+  handlePageUpdate = newPage => {
+    if (newPage <= 0) {
+      return;
+    } else {
+      this.handleDataLoad(newPage)
+      this.setState({currentPage: newPage})
+    }
+  }
   
   render() { 
     return (
-      <SearchBar/>
-
-    }
+      <Container>
+        {/* <SearchBar/> */}
+        <PhotoContainer photos={this.state.photos} />
+        <PaginationBar currentPage={this.state.currentPage} onPageUpdate={this.handlePageUpdate}/>
+      </Container>
+    )
   }
+}
   
 
 export default App;
